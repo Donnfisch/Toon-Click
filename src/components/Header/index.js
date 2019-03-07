b00fe2975909ddca78c927ea6dc7ac278eccc645
donnfisch <mrblix@gmail.com]>
2019-03-06 19:29:40 -0600

    fixed images

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./header.css";
+
+
+const Header = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">Looney Toons Memory</h1>
+    <p className="lead">Click on the Toons to earn points. Click on them more than once and you lose!</p>
+  </div>
+</div>
+);
+export default Header;
