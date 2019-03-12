cabb4838307b0a750116332363b0ab66b1825b07
donnfisch <mrblix@gmail.com]>
2019-03-11 23:22:52 -0500

    Updates

---
+++ precache-manifest.c0ed677628999131757db24c8eed413a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37b603a4268fa5d79a13",
+    "url": "/Toon-Click/static/js/runtime~main.22db924e.js"
+  },
+  {
+    "revision": "c3d8300d3c1c26ef5f3e",
+    "url": "/Toon-Click/static/js/main.605e9c32.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/Toon-Click/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "c3d8300d3c1c26ef5f3e",
+    "url": "/Toon-Click/static/css/main.23d7e9bb.chunk.css"
+  },
+  {
+    "revision": "2f8e3146367f6d620b27e09c7141e17e",
+    "url": "/Toon-Click/index.html"
+  }
+];
